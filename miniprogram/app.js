138f82ca9770e1c3b2d4b3ab98c979ebb030d222
cc-sl <2703023005@qq.com>
2026-05-19 15:30:19 +0800

    feat: 简单修改app.js,实现正常调试

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,34 +1,8 @@
-import { isLoggedIn, getUserInfo, clearAuth } from './utils/storage'
-
 App({
-  globalData: {
-    userInfo: null,
-    isLoggedIn: false,
-  },
-
   onLaunch() {
-    // 启动时恢复登录态
-    const loggedIn = isLoggedIn()
-    this.globalData.isLoggedIn = loggedIn
-    if (loggedIn) {
-      this.globalData.userInfo = getUserInfo()
-    } else {
-      clearAuth()
-    }
+    console.log('小程序启动了')
   },
-
-  /** 登录成功后由登录页调用，同步全局状态 */
-  setLogin(authData) {
-    const { saveAuth } = require('./utils/storage')
-    saveAuth(authData)
-    this.globalData.isLoggedIn = true
-    this.globalData.userInfo = authData.user || null
-  },
-
-  /** 退出登录 */
-  setLogout() {
-    clearAuth()
-    this.globalData.isLoggedIn = false
-    this.globalData.userInfo = null
-  },
+  globalData: {
+    userInfo: null
+  }
 })
