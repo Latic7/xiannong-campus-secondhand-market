c80530ed259d873d8effc715472a9b1ee27e7c9a
cc-sl <2703023005@qq.com>
2026-06-19 15:03:36 +0800

    feat(miniprogram): 全局登录过期处理，过期自动清auth并跳转用户页

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,6 +1,16 @@
+const { setAuthExpiredHandler } = require('./utils/api')
+const { clearAuth } = require('./utils/storage')
+
 App({
   onLaunch() {
     console.log('小程序启动了')
+    setAuthExpiredHandler(() => {
+      clearAuth()
+      wx.showToast({ title: '登录已过期，请重新登录', icon: 'none' })
+      setTimeout(() => {
+        wx.switchTab({ url: '/pages/user/index' })
+      }, 800)
+    })
   },
   globalData: {
     userInfo: null
